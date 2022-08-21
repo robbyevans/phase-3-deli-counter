1e7020c3e17a34b7dd40a6896122fbe4b3e2a16d
Evans Rutto <robbyevans001@gmail.com>
2022-08-21 11:25:45 +0300

    finally

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,41 @@
-# Write your code here.
+# # Write your code here.
+
+katz_deli=[]
+
+
+def take_a_number(array,name)
+  array<< name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+  
+end
+take_a_number(katz_deli,"ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Kent")
+take_a_number(katz_deli,"Mula")
+
+def line(array)
+  if array==[]
+    puts "The line is currently empty."
+  else
+    info=[]
+    pos=1
+    array.each do |item|
+    info<<" #{pos}. #{item}"
+    pos+=1
+    end
+    puts"The line is currently:#{info.join}"
+  end
+end
+
+  line (katz_deli)
+
+def now_serving(array)
+  if array==[]
+    puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{array[0]}."
+  array.shift
+  end
+end
+
+now_serving(katz_deli)
